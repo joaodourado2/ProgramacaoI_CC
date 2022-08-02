fbcec91c06bd392c99ef8ea91b956bd89f95eb4b
joaodourado2 <87993902+joaodourado2@users.noreply.github.com>
2022-08-02 20:39:46 -0300

    variaveis

---
+++ Cadastro.js
@@ -0,0 +1,7 @@
+nomeAluno = "Joao";
+idadeAluno = 22;
+cursoAluno = 'Ciencias da Computação';
+
+alert("Nome do Aluno: " + nomeAluno + "\nIdade Aluno: " + idadeAluno + "\nCurso do Aluno: " + cursoAluno);
+
+sss
